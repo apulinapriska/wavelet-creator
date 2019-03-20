5d7292a00c8a9cdda3f05212e1a6f4795eb53fef
apulinapriska <47569941+apulinapriska@users.noreply.github.com>
2019-03-20 11:57:47 +0700

    Update berlagepulse.m

--- berlagepulse.m
+++ berlagepulse.m
@@ -17,11 +17,12 @@ t=tinput.*(10^6); %convert time to
 f_MHz=f0/(10^6); %convert input frequency to MHz
 size_t=size(t);
 amt_t=size_t(:,2);
+alpha=1/f_MHz;
 %w=A*heaviside(t).*(t.^n).*exp(-alpha*t).*cos(2*pi*f0*t + phi0)
 %A is amplitude, choose so the maximum near 1
     fh=heaviside(t);
     t2=t.^2;
-    p=A*fh.*t2.*exp(-3*(f_MHz)*(t)).*cos((2*pi()*f_MHz*t)+phi0);
+    p=A*fh.*t2.*exp(-alpha*(f_MHz)*(t)).*cos((2*pi()*f_MHz*t)+phi0);
 %normalise the wavelet
 for i=1:amt_t
 pb (i)= p(i) - min(abs(p(:)));
